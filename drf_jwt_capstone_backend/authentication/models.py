c4cb2284aa63ed6a94ca631e5669305ab605dabc
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-12-06 10:23:02 -0600

    added models phone_number, and home_address to models on authentication app

--- drf_jwt_capstone_backend/authentication/models.py
+++ drf_jwt_capstone_backend/authentication/models.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    Middle_Name = models.CharField(max_length=20)
-    Home_Address = models.TextField(max_length=100, default='default')
-    Phone_Number = models.TextField(max_length=12, default='default')
+    middle_name = models.CharField(max_length=20)
+    home_address = models.TextField(max_length=100, default='default')
+    phone_number = models.TextField(max_length=12, default='default')
 
